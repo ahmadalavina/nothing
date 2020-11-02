56c198ff74ea8b53a88e70eb9d47fe8e04d8d15a
Ahmad Alavi <A.Alavi@NBSAfzar.Local>
2020-11-02 10:22:49 +0330

    secound

---
+++ main2.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+const Persons = [
+    { id: 1, fullname: "ahmad alavi" },
+    { id: 2, fullname: "farhad safaee" },
+    { id: 3, fullname: "ahmad tahmasebi" },
+
+];
+fs.writeFileSync("contact.json",JSON.stringify(Persons))
+
+const data = fs.readFileSync('contact.json')
+
+console.log(data)
