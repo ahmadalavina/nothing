f8716b99a1d373eaf7ee799f4c979776dc42d4e5
Ahmad Alavi <A.Alavi@NBSAfzar.Local>
2020-11-02 12:58:14 +0330

    last commit

--- main2.js
+++ main2.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-
+const proc = require('./process')
 const Persons = [
     { id: 1, fullname: "ahmad alavi" },
     { id: 2, fullname: "farhad safaee" },
@@ -11,3 +11,4 @@ fs.writeFileSync("contact.json",JSON.stringify(Persons))
 const data = fs.readFileSync('contact.json')
 
 console.log(data)
+
--- process.js
+++ process.js
@@ -1,1 +1,38 @@
-process.argv.forEach((val, index)=>console.log(`${index}: ${val}`))
+const fs = require("fs")
+const yargs = require('yargs')
+//  var ahmad ="ahmad"
+// console.log(process.argv)
+
+yargs.command({
+    command: "create",
+    describe: "[create new contact]",
+
+    builder: {
+        fullname: {
+            alias: "f",
+            describe: "Person full name ",
+            demandOption: true,
+            type: "string"
+        },
+        phone: {
+            alias: "p",
+            describe: "Person full number ",
+            demandOption: true,
+            type: "number"
+        }
+    },
+
+    handler({ fullname, phone, email }) {
+        console.log(fullname, phone, email)
+    },
+})
+yargs.parse()
+// var argoment = yargs.argv
+// console.log(yargs.argv)
+
+// var MyJSON =JSON.stringify(argoment)
+
+// fs.writeFileSync('txt.txt' ,MyJSON,(err)=>{
+//     if(err) throw err
+//     console.log("your file is created")
+// })
